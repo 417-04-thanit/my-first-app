7a63ebad50479da5e94d6f1f4a934d9c9e334c4e
417-04-thanit <54222@yupparaj.ac.th>
2026-09-09 11:51:40 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -108,11 +108,11 @@ ans2 = st.text_input(
 )
 ans3 = st.text_input(
     "ข้อ 3: There are one `o _ _ _ g e` on the desk. 🍊",
-    value=st.session_state.ans1_val,
+    value=st.session_state.ans3_val,
 )
 ans4 = st.text_input(
     "ข้อ 4: I love to eat `b a _ _ n a`. 🍌",
-    value=st.session_state.ans1_val,
+    value=st.session_state.ans4_val,
 )
 
 # อัปเดตค่าล่าสุดเข้าตัวแปร
