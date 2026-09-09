4a710d7b6881d1403c7da158ebc9ba00329dea6b
417-04-thanit <54222@yupparaj.ac.th>
2026-09-09 11:44:46 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -61,7 +61,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
     else:
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
-  if u_ans4 == "banana":
+    if u_ans4 == "banana":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
